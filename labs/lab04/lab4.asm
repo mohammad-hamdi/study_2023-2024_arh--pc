2c08ae5ac96f6dee522e9dfbf0e10ad9716227c4
AhmadFarg96 <a.farg22@icloud.com>
2023-12-17 15:53:30 +0300

    feat(main): add files lab-4

--- labs/lab04/lab4.asm
+++ labs/lab04/lab4.asm
@@ -1,14 +1,14 @@
 SECTION .data
-hello:db "Хамди Мохаммад",0xa 
-helloLen:   equ $ - hello
+    hello: DB 'ахмед метвалли',10
+    hellolen:   EQU $- hello
 SECTION .text
-global _start           
-_start:                 
-mov eax, 4      
-mov ebx, 1    
-mov ecx, hello
-mov edx, helloLen
-int 0x80        
-mov eax, 1       
-mov ebx, 0      
-int 0x80        
+    GLOBAL _start
+_start:
+    mov eax,4
+    mov ebx,1 
+    mov ecx,hello
+    mov edx,hellolen
+    int 80h
+    mov eax,1
+    mov ebx,0
+    int 80h
